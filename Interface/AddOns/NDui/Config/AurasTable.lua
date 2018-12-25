e2e5d5505a1190af43e78797f496c1b538ed0342
niGhtla1 <45828349+niGhtla1@users.noreply.github.com>
2018-12-25 16:37:32 +0800

    冷却计时向下增长

--- Interface/AddOns/NDui/Config/AurasTable.lua
+++ Interface/AddOns/NDui/Config/AurasTable.lua
@@ -9,12 +9,12 @@ local groups = {
 	["Target Aura"] = {"RIGHT", 5, "ICON", 36, C.Auras.TargetAuraPos},
 	["Special Aura"] = {"LEFT", 5, "ICON", 36, C.Auras.SpecialPos},
 	["Focus Aura"] = {"RIGHT", 5, "ICON", 35, C.Auras.FocusPos},
-	["Spell Cooldown"] = {"UP", 5, "BAR", 18, C.Auras.CDPos, 150},
+	["Spell Cooldown"] = {"DOWN", 5, "BAR", 18, C.Auras.CDPos, 150},
 	["Enchant Aura"] = {"LEFT", 5, "ICON", 36, C.Auras.EnchantPos},
 	["Raid Buff"] = {"LEFT", 5, "ICON", 42, C.Auras.RaidBuffPos},
 	["Raid Debuff"] = {"RIGHT", 5, "ICON", 42, C.Auras.RaidDebuffPos},
 	["Warning"] = {"RIGHT", 5, "ICON", 42, C.Auras.WarningPos},
-	["InternalCD"] = {"UP", 5, "BAR", 18, C.Auras.InternalPos, 150},
+	["InternalCD"] = {"DOWN", 5, "BAR", 18, C.Auras.InternalPos, 150},
 }
 
 -- AuraWatch
